ccd128e44065b8912e902553779b40e7ad85fb76
MartinStokholm <msldk6a@gmail.com>
2024-11-13 15:57:20 +0100

    Started moving stuff to .env

--- src/config/settings.js
+++ src/config/settings.js
@@ -2,20 +2,22 @@
  * @typedef {settings} Settings
  */
 
+const nlmKey = import.meta.env.VITE_NLM_KEY;
+const openAiBaseUrl = import.meta.env.VITE_BASE_URL;
 export const aiURL = "https://nempubmed.dk/om-nempubmed";
 
 export const settings = {
   client: "videncenterfordiabetes",
   nlm: {
     email: "admin@videncenterfordiabetes.dk",
-    key: "258a604944c9858b96739c730cd6a579c908",
+    key: nlmKey,
     myncbishare: "videncenterfordiabetes",
   },
   openAi: {
     domain: "diabetes",
     useAi: true,
     usePDFsummary: true, // If true, the AI will summarize the PDFs
-    baseUrl: "http://localhost:7071", // PROD "https://qpm-openai-service.azurewebsites.net",
+    baseUrl: openAiBaseUrl,
     longAbstractLengthLimit: 5000, // The combined abstract length of the selected articles in characters
     waitTimeDisclaimerDelay: 5000, // The amount of milliseconds before the wait time disclaimer is shown
   },
