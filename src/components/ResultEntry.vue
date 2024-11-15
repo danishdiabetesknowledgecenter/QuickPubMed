a99e69381da7b174355e5a94149a82a5fccb1e65
MartinStokholm <msldk6a@gmail.com>
2024-11-15 09:26:50 +0100

    Side effect now as method calls

--- src/components/ResultEntry.vue
+++ src/components/ResultEntry.vue
@@ -1046,11 +1046,11 @@ export default {
         this.unpaywallResponse["best_oa_location"]["url_for_pdf"];
 
       if (!url_for_pdf) {
-        this.pdfUrl = undefined;
+        this.setPdfUrl(undefined);
         return false;
       }
 
-      this.pdfUrl = url_for_pdf;
+      this.setPdfUrl(url_for_pdf);
       console.log("url_for_pdf:", this.pdfUrl);
 
       return true;
@@ -1066,11 +1066,11 @@ export default {
         this.unpaywallResponse["best_oa_location"]["url_for_landing_page"];
 
       if (!url_for_landing_page) {
-        this.htmlUrl = undefined;
+        this.setHtmlUrl(undefined);
         return false;
       }
 
-      this.htmlUrl = url_for_landing_page;
+      this.setHtmlUrl(url_for_landing_page);
       console.log("url_for_landing_page", this.htmlUrl);
       return true;
     },
@@ -1389,6 +1389,12 @@ export default {
         this.isResourceAllowed = false;
         return false;
       }
+    },
+    setPdfUrl(url) {
+      this.pdfUrl = url;
+    },
+    setHtmlUrl(url) {
+      this.htmlUrl = url;
     },
     setAbstract() {
       console.log(`my id is: ${this.id}`);
