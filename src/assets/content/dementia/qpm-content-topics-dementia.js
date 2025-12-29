557c10c357cfbe14691d797a8db41fc6667498e1
Videncenter for Diabetes <admin@videncenterfordiabetes.dk>
2025-12-29 23:15:09 +0100

    Update qpm-content-topics-dementia.js

--- src/assets/content/dementia/qpm-content-topics-dementia.js
+++ src/assets/content/dementia/qpm-content-topics-dementia.js
@@ -36,7 +36,7 @@ export const topics = [
     id: "S000",
     groupname: "S000",
     translations: {
-      dk: "Skabelonkategori",
+      dk: "Skabelonkategorix",
       en: "Template category",
     },
     ordering: { dk: 0, en: 0 },
